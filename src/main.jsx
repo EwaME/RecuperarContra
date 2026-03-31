e5a3f8b6bad89523381c4ab2610f754a71dcb219
EwaME <edwardmaradiagaespinal@gmail.com>
2026-03-31 00:41:51 -0600

    Commit Bueno

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import RecuperarPassword from './RecuperarPassword.jsx';
+import NuevaPassword from './NuevaPassword.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RecuperarPassword />} />
+        <Route path="/" element={<NuevaPassword />} />
         
         <Route path="/login" element={
           <div className="min-h-screen flex items-center justify-center bg-[#F8FAFC]">
